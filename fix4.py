28fe69af66cf97c81329252184a35182f799a3c7
KAlpAkbaba <kadir@jarvis.com>
2026-04-26 12:01:23 +0000

    fix: matematik, not alma, hatirlatici icerik duzeltmeleri

--- fix4.py
+++
@@ -1,7 +0,0 @@
-f = open('/app/core/assistant.py', 'r')
-content = f.read()
-f.close()
-
-idx = content.find('not_icerik') 
-print('not_icerik override:')
-print(repr(content[idx-50:idx+200]))
--- fix8.py
+++
@@ -1,6 +0,0 @@
-f = open('/app/core/assistant.py', 'r')
-content = f.read()
-f.close()
-
-idx = content.find('HATIRLATICI":')
-print(repr(content[idx:idx+400]))
